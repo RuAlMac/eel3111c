c997c48476fe0721e10ebd45465edf2587d21486
Russell MacGregor <110684528+RuAlMac@users.noreply.github.com>
2024-10-31 11:58:46 -0400

    Important! Updated code to be compatible with TI-Nspire
    TI-Nspire only supports an older version of python. The following elements are not supported. Alternatives to these can be found in the .py file with my functions.
    -> match/case statements
    -> fstrings
    -> if __name__ == __main__

--- Exam2/exam2_lib.py
+++ Exam2/exam2_lib.py
@@ -3,6 +3,12 @@
 #       - rlc_response()
 #           > Inputs: a circuit's resistance, capacitance, and inducatance and if it is parallel/series and if looking for natural/step response
 #           > Outputs: whether overdamped, underdamped, critically damped, roots of char eq, neper frequency, resonant frequency, all associated equations
+#
+#   Important:
+#       The TI-NSpire runs an older version of python, and therefore does not support the following python elements
+#           > Match/Case statements (use if/elif/else instead)
+#           > fstrings (alternative example: myStr = "value1 = {}, value2 = {}".format(v1, v2) )
+#           > __name__ == __main__ (just put code to be automatically run at the very end of the .py file outside any functions)
 
 import math
 import cmath
@@ -65,22 +71,22 @@ def rlc_response():
         s1,s2 = calculateRoots(a,w)
 
         # Set up characteristic equation
-        charEq = f"s^2 + {1/(r*c):.2e}*s + {1/(l*c):.2e} = 0"
+        charEq = "s^2 + {:.2e}*s + {:.2e} = 0".format(1/(r*c), 1/(l*c))
 
         # Set up equations v(t) [v_t], v(0+) [v_t_0], dv(0+)/dt [deriv]
-        match state:
-            case "overdamped":
-                eq1 = f"v(t) = A1*e^({s1:.2e}*t) + A2*e^({s2:.2e}*t), t>=0"
-                eq2 = f"v(0+) = A1 + A2 = V0"
-                eq3 = f"dv(0+)/dt = {s1:.2e}*A1 + {s2:.2e}*A2 = -(V0 + R*I0)/(R*C)"
-            case "underdamped":
-                eq1 = f"v(t) = B1*e^({-1*a:.2e}*t)*cos({w}*t) + B2*e^({-1*a:.2e}*t)*sin({w:.2e}*t), t>=0"
-                eq2 = f"v(0+) = B1 = V0"
-                eq3 = f"dv(0+)/dt = {-1*a:.2e}*B1 + {w:.2e}*B2 = -(V0 + R*I0)/(R*C)"
-            case "critically damped":
-                eq1 = f"v(t) = D1*t*e^({-1*a:.2e}*t) + D2*e^({-1*a:.2e}*t), t>=0"
-                eq2 = f"v(0+) = D2 = V0"
-                eq3 = f"dv(0+)/dt = D1 - {a:.2e}*D2 = -(V0 + R*I0)/(R*C)"
+        if state == "overdamped":
+            eq1 = "v(t) = A1*e^({:.2e}*t) + A2*e^({:.2e}*t), t>=0".format(s1, s2)
+            eq2 = "v(0+) = A1 + A2 = V0"
+            eq3 = "dv(0+)/dt = {:.2e}*A1 + {:.2e}*A2 = -(V0 + R*I0)/(R*C)".format(s1, s2)
+        elif state == "underdamped":
+            eq1 = "v(t) = B1*e^({:.2e}*t)*cos({:.2e}*t) + B2*e^({:.2e}*t)*sin({:.2e}*t), t>=0".format(-a,w,-a,w)
+            eq2 = "v(0+) = B1 = V0"
+            eq3 = "dv(0+)/dt = {:.2e}*B1 + {:.2e}*B2 = -(V0 + R*I0)/(R*C)".format(-a,w)
+        elif state == "critically damped":
+            eq1 = "v(t) = D1*t*e^({:.2e}*t) + D2*e^({:.2e}*t), t>=0".format(-a,-a)
+            eq2 = "v(0+) = D2 = V0"
+            eq3 = "dv(0+)/dt = D1 - {:.2e}*D2 = -(V0 + R*I0)/(R*C)".format(a)
+
 
 
     if ( (orientation == "p") and (typeResponse == "s") ): #parallel step response
@@ -96,22 +102,21 @@ def rlc_response():
         s1,s2 = calculateRoots(a,w)
 
         # Set up characteristic equation
-        charEq = f"s^2 + {1/(r*c):.2e}*s + {1/(l*c):.2e} = 1/{l*c:.2e}"
+        charEq = "s^2 + {:.2e}*s + {:.2e} = 0".format(1/(r*c), 1/(l*c))
 
         # Set up equations iL(t) [iL_t], iL(0+) [iL_t_0], diL(0+)/dt [deriv] 
-        match state:
-            case "overdamped":
-                eq1 = f"iL(t) = I_f + A1*e^({s1:.2e}*t) + A2*e^({s2:.2e}*t), t>=0"
-                eq2 = f"iL(0+) = I_f + A1 + A2 = I_0"
-                eq3 = f"diL(0+)/dt = {s1:.2e}*A1 + {s2:.2e}*A2 = (V_0/L)"
-            case "underdamped":
-                eq1 = f"iL(t) = I_f + B1*e^({-1*a:.2e}*t)*cos({w:.2e}*t) + B2*e^({-1*a:.2e}*t)*sin({w:.2e}*t), t>=0"
-                eq2 = f"iL(0+) = I_f + B1 = I_0"
-                eq3 = f"diL(0+)/dt = {-1*a:.2e}*B1 + {w:.2e}*B2 = (V_0/L)"
-            case "critically damped":
-                eq1 = f"iL(t) = I_f + D1*t*e^({-1*a:.2e}*t) + D2*e^({-1*a:.2e}*t), t>=0"
-                eq2 = f"iL(0+) = I_f + D2 = I_0"
-                eq3 = f"diL(0+)/dt = D1 - {a:.2e}*D2 = (V_0/L)"
+        if state == "overdamped":
+            eq1 = "iL(t) = I_f + A1*e^({:.2e}*t) + A2*e^({:.2e}*t), t>=0".format(s1, s2)
+            eq2 = "iL(0+) = I_f + A1 + A2 = I_0"
+            eq3 = "diL(0+)/dt = {:.2e}*A1 + {:.2e}*A2 = (V_0/L)".format(s1, s2)
+        elif state == "underdamped":
+            eq1 = "iL(t) = I_f + B1*e^({:.2e}*t)*cos({:.2e}*t) + B2*e^({:.2e}*t)*sin({:.2e}*t), t>=0".format(-a, w, -a, w)
+            eq2 = "iL(0+) = I_f + B1 = I_0"
+            eq3 = "diL(0+)/dt = {:.2e}*B1 + {:.2e}*B2 = (V_0/L)".format(-a, w)
+        elif state == "critically damped":
+            eq1 = "iL(t) = I_f + D1*t*e^({:.2e}*t) + D2*e^({:.2e}*t), t>=0".format(-a,-a)
+            eq2 = "iL(0+) = I_f + D2 = I_0"
+            eq3 = "diL(0+)/dt = D1 - {:.2e}*D2 = (V_0/L)".format(a)
 
 
     if ( (orientation == "s") and (typeResponse == "n") ): #series natural response
@@ -127,22 +132,22 @@ def rlc_response():
         s1,s2 = calculateRoots(a,w)
 
         # Set up characteristic equation
-        charEq = f"s^2 + {r/l:.2e}*s + {1/(l*c):.2e} = 0"
+        charEq = "s^2 + {:.2e}*s + {:.2e} = 0".format(r/l, 1/(l*c))
 
-        # Set up equations iL(t) [iL_t], iL(0+) [iL_t_0], diL(0+)/dt [deriv] 
-        match state:
-            case "overdamped":
-                eq1 = f"iL(t) = A1*e^({s1:.2e}*t) + A2*e^({s2:.2e}*t), t>=0"
-                eq2 = f"iL(0+) = A1 + A2 = I_0"
-                eq3 = f"diL(0+)/dt = {s1:.2e}*A1 + {s2:.2e}*A2 = -(1/L)*(R*I_0 + V_0)"
-            case "underdamped":
-                eq1 = f"iL(t) = B1*e^({-1*a:.2e}*t)*cos({w:.2e}*t) + B2*e^({-1*a:.2e}*t)*sin({w:.2e}*t), t>=0"
-                eq2 = f"iL(0+) = B1 = I_0"
-                eq3 = f"diL(0+)/dt = {-1*a:.2e}*B1 + {w:.2e}*B2 = -(1/L)*(R*I_0 + V_0)"
-            case "critically damped":
-                eq1 = f"iL(t) = D1*t*e^({-1*a:.2e}*t) + D2*e^({-1*a:.2e}*t), t>=0"
-                eq2 = f"iL(0+) = D2 = I_0"
-                eq3 = f"diL(0+)/dt = D1 - {a:.2e}*D2 = -(1/L)*(R*I_0 + V_0)"
+        # Set up equations iL(t) [iL_t], iL(0+) [iL_t_0], diL(0+)/dt [deriv]
+        if state == "overdamped":
+            eq1 = "iL(t) = A1*e^({:.2e}*t) + A2*e^({:.2e}*t), t>=0".format(s1, s2)
+            eq2 = "iL(0+) = A1 + A2 = I_0"
+            eq3 = "diL(0+)/dt = {:.2e}*A1 + {:.2e}*A2 = -(1/L)*(R*I_0 + V_0)".format(s1,s2)
+        elif state == "underdamped":
+            eq1 = "iL(t) = B1*e^({:.2e}*t)*cos({:.2e}*t) + B2*e^({:.2e}*t)*sin({:.2e}*t), t>=0".format(-a,w-a,w)
+            eq2 = "iL(0+) = B1 = I_0"
+            eq3 = "diL(0+)/dt = {:.2e}*B1 + {:.2e}*B2 = -(1/L)*(R*I_0 + V_0)".format(-a,w)
+        elif state == "critically damped":
+            eq1 = "iL(t) = D1*t*e^({:.2e}*t) + D2*e^({:.2e}*t), t>=0".format(-a,-a)
+            eq2 = "iL(0+) = D2 = I_0"
+            eq3 = "diL(0+)/dt = D1 - {:.2e}*D2 = -(1/L)*(R*I_0 + V_0)".format(a)
+                
 
     
     if ( (orientation == "s") and (typeResponse == "s") ): #series step response
@@ -158,24 +163,21 @@ def rlc_response():
         s1,s2 = calculateRoots(a,w)
 
         # Set up characteristic equation
-        charEq = f"s^2 + {r/l:.2e}*s + {1/(l*c):.2e} = V/{l*c:.2e}"
+        charEq = "s^2 + {:.2e}*s + {:.2e} = V/{:.2e}".format(r/l, 1/(l*c), l*c)
 
         # Set up equations vC(t) [vC_t], vC(0+) [vC_t_0], dvC(0+)/dt [deriv]
-        match state:
-            case "overdamped":
-                eq1 = f"vC(t) = V_f + A1*e^({s1:.2e}*t) + A2*e^({s2:.2e}*t), t>=0"
-                eq2 = f"vC(0+) = V_f + A1 + A2 = V_0"
-                eq3 = f"dvC(0+)/dt = {s1:.2e}*A1 + {s2:.2e}*A2 = (I_0/C)"
-            case "underdamped":
-                eq1 = f"vC(t) = V_f + B1*e^({-1*a:.2e}*t)*cos({w:.2e}*t) + B2*e^({-1*a:.2e}*t)*sin({w:.2e}*t), t>=0"
-                eq2 = f"vC(0+) = V_f + B1 = V_0"
-                eq3 = f"dvC(0+)/dt = {-1*a:.2e}*B1 + {w:.2e}*B2 = (I_0/C)"
-                pass
-            case "critically damped":
-                eq1 = f"vC(t) = V_f + D1*t*e^({-1*a:.2e}*t) + D2*e^({-1*a:.2e}*t), t>=0"
-                eq2 = f"vC(0+) = V_f + D2 = V_0"
-                eq3 = f"dvC(0+)/dt = D1 - {a:.2e}*D2 = (I_0/C)"
-                pass
+        if state == "overdamped":
+            eq1 = "vC(t) = V_f + A1*e^({:.2e}*t) + A2*e^({:.2e}*t), t>=0".format(s1, s2)
+            eq2 = "vC(0+) = V_f + A1 + A2 = V_0"
+            eq3 = "dvC(0+)/dt = {:.2e}*A1 + {:.2e}*A2 = (I_0/C)".format(s1, s2)
+        elif state == "underdamped":
+            eq1 = "vC(t) = V_f + B1*e^({:.2e}*t)*cos({:.2e}*t) + B2*e^({:.2e}*t)*sin({:.2e}*t), t>=0".format(-a, w, -a, w)
+            eq2 = "vC(0+) = V_f + B1 = V_0"
+            eq3 = "dvC(0+)/dt = {:.2e}*B1 + {:.2e}*B2 = (I_0/C)".format(-a,w)
+        elif state == "critically damped":
+            eq1 = "vC(t) = V_f + D1*t*e^({:.2e}*t) + D2*e^({:.2e}*t), t>=0".format(-a, -a)
+            eq2 = "vC(0+) = V_f + D2 = V_0"
+            eq3 = "dvC(0+)/dt = D1 - {:.2e}*D2 = (I_0/C)".format(a)
 
     # Print results
     if (orientation == "p"):
@@ -189,18 +191,24 @@ def rlc_response():
         type_str = "Step"
 
     print("Outputs:")
-    print(f"\tCircuit is {state}")
-    print(f"\tneper freq = {a:.2e}")
-    print(f"\tresonant freq = {w:.2e}")
-    print(f"\ts1 = {s1:.2e}")
-    print(f"\ts2 = {s2:.2e}")
-    print(f"\tChar Eq: {charEq}")
-    print(f"\t{eq1}")
-    print(f"\t{eq2}")
-    print(f"\t{eq3}")
+    print("\tCircuit is {}".format(state))
+    print("\tneper freq = {:.2e}".format(a))
+    print("\tresonant freq = {:.2e}".format(w))
+    print("\ts1 = {:.2e}".format(s1))
+    print("\ts2 = {:.2e}".format(s2))
+    print("\tChar Eq: {}".format(charEq))
+    print("\t{}".format(eq1))
+    print("\t{}".format(eq2))
+    print("\t{}".format(eq3))
 
     return
 
 
-if __name__ == "__main__":
+# *****Code to execute when this script is run*****
+print("Select a function to use:")
+print("\t1) rlc_response")
+
+option = input("Function: ")
+
+if option == 1:
     rlc_response()
